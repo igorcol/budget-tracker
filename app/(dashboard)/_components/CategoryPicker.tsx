7f0f76931e73b189a6901fa100f9b1664bd3c55c
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-03 17:45:00 -0300

    feat createTransaction dialog

---
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+
+interface props {
+  
+}
+
+const CategoryPicker = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryPicker
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,41 +1,50 @@
-import { Button } from '@/components/ui/button';
-import { prisma } from '@/lib/prisma';
-import { currentUser } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 
 const page = async () => {
   const user = await currentUser();
-  if (!user) redirect("sign-in")
+  if (!user) redirect("sign-in");
 
   const userSettings = await prisma.userSettings.findUnique({
     where: {
-      userId: user.id
-    }
-  })
+      userId: user.id,
+    },
+  });
 
-  if (!userSettings) redirect("/wizard")
+  if (!userSettings) redirect("/wizard");
 
   return (
-    <div className='h-full bg-background'>
+    <div className="h-full bg-background">
       <div className="border-b bg-card flex flex-row items-center justify-center px-6">
         <div className="w-full flex flex-wrap items-center justify-between gap-6 py-8">
-          <p className="text-3xl font-bold">
-            Hello, {user.firstName}! 👋
-          </p>
+          <p className="text-3xl font-bold">Hello, {user.firstName}! 👋</p>
 
           <div className="flex items-center gap-3">
-            <Button  className='border border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white'>
-              + New Income
-            </Button>
-            <Button  className='border border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white'>
-              + New Expense
-            </Button>
+            <CreateTransactionDialog
+              trigger={
+                <Button className="border border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white">
+                  + New Income
+                </Button>
+              }
+              type="income"
+            />
+            <CreateTransactionDialog
+              trigger={
+                <Button className="border border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white">
+                  + New Expense
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
