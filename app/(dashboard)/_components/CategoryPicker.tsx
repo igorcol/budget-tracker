5bce56b1b26ce8385a210655afa21edf5d150352
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-03 19:59:15 -0300

    add category picker

--- app/(dashboard)/_components/CategoryPicker.tsx
+++ app/(dashboard)/_components/CategoryPicker.tsx
@@ -1,12 +1,55 @@
 "use client"
 
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Command, CommandInput } from '@/components/ui/command'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import { Category } from '@/lib/generated/prisma'
+import { TransactionType } from '@/lib/types'
+import { useQuery } from '@tanstack/react-query'
+import React, { useState } from 'react'
 
+interface props {
+  type: TransactionType
+}
 
-const CategoryPicker = () => {
+const CategoryPicker = ({type}: props) => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  // Fetch categories
+  const categoriesQuery = useQuery({
+    queryKey: ["categories", type],
+    queryFn: () => fetch(`/api/categories?type=${type}`)
+    .then(res => res.json())
+  });
+
+  const selectedCategory = categoriesQuery.data?.find(
+    (category: Category) => category.name === value
+  );
+
   return (
-    <div>
-      CategoryPicker
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button variant={"outline"} role='combobox' aria-expanded={open} className='w-[200px] justify-between'>
+          {selectedCategory ? <CategoryRow category={selectedCategory}/> : "Select Category" }
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className='w-[200px] p-0'>
+        <Command onSubmit={e => {e.preventDefault()}}>
+          <CommandInput
+            placeholder='Search Category...'
+          />
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+function CategoryRow({category}: {category: Category}) {
+  return (
+    <div className="flex items-center gap-2">
+      <span role='img'>{category.icon}</span>
+      <span>{category.name}</span>
     </div>
   )
 }
