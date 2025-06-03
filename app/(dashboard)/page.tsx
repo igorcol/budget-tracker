83f3df6f6591aa11af6f4a707794bed8ac04a6a4
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-03 17:18:27 -0300

    fix dashboard header style

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -18,8 +18,8 @@ const page = async () => {
 
   return (
     <div className='h-full bg-background'>
-      <div className="border-b bg-card">
-        <div className="container flex flex-wrap items-center justify-between gap-6 px-6 py-8">
+      <div className="border-b bg-card flex flex-row items-center justify-center px-6">
+        <div className="w-full flex flex-wrap items-center justify-between gap-6 py-8">
           <p className="text-3xl font-bold">
             Hello, {user.firstName}! 👋
           </p>
