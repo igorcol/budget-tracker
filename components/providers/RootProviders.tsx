6e3532fd4b3b403c4431ec666f4d8858ea795d7e
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-03 15:54:50 -0300

    tanstack query setup

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,10 +1,15 @@
 "use client"
 
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function RootProviders({children}: {children: ReactNode}) {
-    return <ThemeProvider
+    const [queryClient] = useState(() => new QueryClient({}));
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider
                 attribute="class"
                 defaultTheme="dark"
                 enableSystem
@@ -12,6 +17,9 @@ function RootProviders({children}: {children: ReactNode}) {
             >
                 {children}
             </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    )
 }
 
 export default RootProviders
