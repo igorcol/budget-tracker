c26e8690eca4a653384a9b2c98c83d56396b3eee
Igor - NOTE_2 <igor.colombini@gmail.com>
2025-06-03 16:23:13 -0300

    create and implement SkeletonWrapper in ComboBox

--- components/CurrencyComboBox.tsx
+++ components/CurrencyComboBox.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -10,72 +10,77 @@ import {
   CommandInput,
   CommandItem,
   CommandList,
-} from "@/components/ui/command"
-import {
-  Drawer,
-  DrawerContent,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
+} from "@/components/ui/command";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import { useMediaQuery } from "@/hooks/use-media-query"
-import { Currencies, Currency } from "@/lib/currencies"
-import { useQuery } from "@tanstack/react-query"
-
+} from "@/components/ui/popover";
+import { useMediaQuery } from "@/hooks/use-media-query";
+import { Currencies, Currency } from "@/lib/currencies";
+import { useQuery } from "@tanstack/react-query";
+import SkeletonWrapper from "./SkeletonWrapper";
 
 export function CurrencyComboBox() {
-  const [open, setOpen] = React.useState(false)
-  const isDesktop = useMediaQuery("(min-width: 768px)")
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const [selectedOption, setSelectedOption] = React.useState<Currency | null>(
     null
-  )
+  );
 
   const userSettings = useQuery({
     queryKey: ["userSettings"],
-    queryFn: () => fetch("/api/user-settings").then(res => res.json())
-  })
-  
+    queryFn: () => fetch("/api/user-settings").then((res) => res.json()),
+  });
 
   if (isDesktop) {
     return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-start">
-            {selectedOption ? <>{selectedOption.label}</> : <>Set Currency</>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0" align="start">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </PopoverContent>
-      </Popover>
-    )
+      <SkeletonWrapper isLoading={userSettings.isFetching}>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button variant="outline" className="w-full justify-start">
+              {selectedOption ? <>{selectedOption.label}</> : <>Set Currency</>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[200px] p-0" align="start">
+            <OptionList
+              setOpen={setOpen}
+              setSelectedOption={setSelectedOption}
+            />
+          </PopoverContent>
+        </Popover>
+      </SkeletonWrapper>
+    );
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-start">
-          {selectedOption ? <>{selectedOption.label}</> : <>Set Currency</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mt-4 border-t">
-          <OptionList setOpen={setOpen} setSelectedOption={setSelectedOption} />
-        </div>
-      </DrawerContent>
-    </Drawer>
-  )
+    <SkeletonWrapper isLoading={userSettings.isFetching}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <Button variant="outline" className="w-full justify-start">
+            {selectedOption ? <>{selectedOption.label}</> : <>Set Currency</>}
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className="mt-4 border-t">
+            <OptionList
+              setOpen={setOpen}
+              setSelectedOption={setSelectedOption}
+            />
+          </div>
+        </DrawerContent>
+      </Drawer>
+    </SkeletonWrapper>
+  );
 }
 
 function OptionList({
   setOpen,
   setSelectedOption,
 }: {
-  setOpen: (open: boolean) => void
-  setSelectedOption: (status: Currency | null) => void
+  setOpen: (open: boolean) => void;
+  setSelectedOption: (status: Currency | null) => void;
 }) {
   return (
     <Command>
@@ -89,9 +94,10 @@ function OptionList({
               value={currency.value}
               onSelect={(value) => {
                 setSelectedOption(
-                  Currencies.find((priority) => priority.value === value) || null
-                )
-                setOpen(false)
+                  Currencies.find((priority) => priority.value === value) ||
+                    null
+                );
+                setOpen(false);
               }}
             >
               {currency.label}
@@ -100,5 +106,5 @@ function OptionList({
         </CommandGroup>
       </CommandList>
     </Command>
-  )
+  );
 }
